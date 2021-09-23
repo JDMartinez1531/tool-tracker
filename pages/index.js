6419c69f7cb525a06a04c7ecf4f1773b582e68ec
JDMartinez1531 <63159799+JDMartinez1531@users.noreply.github.com>
2021-09-22 22:09:06 -0500

    MUI setup for nextjs

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Map from "../components/Map"
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import Map from "../components/Map";
+
 
 export default function Home() {
   return (
---
+++ pages/test.js
@@ -0,0 +1,20 @@
+import Container from "@mui/material/Container";
+
+const Test = () => {
+  return (
+    <Container maxWidth="sm">
+    <h1>Home Page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos,
+        cupiditate! Odio, aliquam soluta vel, eum illum corrupti incidunt nobis
+        porro mollitia itaque reiciendis. Aut, minus dolore! Delectus pariatur
+        praesentium dolorem? In at, quibusdam vero eligendi provident veritatis
+        ipsam suscipit nisi similique nulla est magni harum. Cumque maiores eos
+        alias, aperiam ea deleniti voluptatem culpa a perferendis accusantium,
+        necessitatibus velit laborum. Molestias, reprehenderit accusantium. Ad
+        ipsa maiores, animi labore est voluptate eos aperiam iste adipisci sed
+        dolor consequatur dolore provident tenetur ipsum! Velit laudantium</p>
+    </Container>
+  );
+};
+
+export default Test;
